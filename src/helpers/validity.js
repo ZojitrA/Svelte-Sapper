d8736b0cea24d1e9550b17e0c8e1d9791f8906f0
Soji <ahsojitra@gmail.com>
2019-11-18 01:16:56 -0500

    shift to sapper

--- src/helpers/validity.js
+++
@@ -1,5 +0,0 @@
-
-export function testEmail(val){
-  let what = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\\b");
-  return what.test(val);
-}
